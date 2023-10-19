c00dc7c163e2e410d0a0c9d56ab120ab34d29dfe
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-10-19 12:50:57 +0530

    Landing page, and Cards done

--- pages/components/LandingSection.js
+++ pages/components/LandingSection.js
@@ -1,7 +1,9 @@
-import { Button } from "flowbite-react";
+import { Button, Card } from "flowbite-react";
 import { React, useState } from "react";
-import leftArrow from "../../assets/lefticon.png";
+import ActiveLeftArrow from "../../assets/activelefticon.svg";
+import InActiveLeftArrow from "../../assets/inactivelefticon.svg";
 import illustration from "../../assets/illustration.png";
+import cardImg from "../../assets/CardImg.png";
 import Image from "next/image";
 
 const LandingSection = () => {
@@ -14,10 +16,52 @@ const LandingSection = () => {
     setCurBtn(2);
   };
 
+    const announcementData = [
+      {
+        img: cardImg,
+        head: "Module Name ",
+        desc: "In this part, we will familiarize ourselves with the practicalities of taking the course.",
+      },
+      {
+        img: cardImg,
+        head: "Module Name",
+        desc: "In this part, we will familiarize ourselves with the practicalities of taking the course.",
+      },
+      {
+        img: cardImg,
+        head: "Module Name",
+        desc: "In this part, we will familiarize ourselves with the practicalities of taking the course.",
+      },
+    ];
+
+  const moduleCard = (
+    <div className='flex justify-evenly rounded-full mx-8 text-white'>
+      {announcementData.map((announcement, index) => (
+        <div
+          className='bg-[rgba(0, 0, 0, 0.16)] border border-[rgba(255, 255, 255, 0.36)] rounded-2xl w-[30%] ease-in-out my-5 mx-2 duration-200'
+          key={index}
+        >
+          <div className='m-1'>
+            <Image src={announcement.img} alt='med' />
+            <div className='px-3 rounded-b-2xl'>
+              <div className='text-2xl pt-6 pb-1'>{announcement.head}</div>
+              <div className='text-[#CECECE] pb-6'>{announcement.desc}</div>
+            </div>
+            <div className='mb-2'>
+              <Button className='w-full bg-white'>
+                <div className='text-center text-black text-xl'>Start Module</div>
+              </Button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <div className='mt-20'>
       <div>
-        <div className="flex justify-between ">
+        <div className='flex justify-between '>
           <div className='w-1/2 text-white'>
             <div className='text-5xl '>
               Unlocking the Power of Solana with Solidity Smart Contracts
@@ -40,13 +84,12 @@ const LandingSection = () => {
                       curBtn === 1 && "bg-white text-black"
                     }`}
                   >
-                    <Image
-                      alt='img'
-                      src={leftArrow}
-                      width={25}
-                      height={25}
-                      className='pr-2'
-                    />
+                    {curBtn == 1 ? (
+                      <ActiveLeftArrow className='mr-3' />
+                    ) : (
+                      <InActiveLeftArrow className='mr-3' />
+                    )}
+
                     <span>Start Course</span>
                   </Button>
                 </div>
@@ -64,11 +107,20 @@ const LandingSection = () => {
               </div>
             </div>
           </div>
-          <div className='w-1/2'>
-            <Image src={illustration} alt="illustration"></Image>
+          <div className='w-1/2 '>
+            <div className='flex justify-end'>
+              <Image
+                src={illustration}
+                alt='illustration'
+                className='fill-white'
+              />
+            </div>
           </div>
         </div>
       </div>
+
+      {/*  */}
+      <div className="mt-20">{moduleCard}</div>
     </div>
   );
 };
