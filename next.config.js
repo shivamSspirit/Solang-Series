f977fda01f92185dfa3a9ff31c1e01be49680c15
shivam <shivam.spirite3@outlook.com>
2023-10-27 02:24:51 +0530

    skip lint when build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ module.exports = {
   images: {
     domains: ["reqres.in"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
