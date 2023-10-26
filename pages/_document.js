62255b9ad1a125e95c9a1b81aab6979e98d932f8
shivam <shivam.spirite3@outlook.com>
2023-10-27 01:53:07 +0530

    add markdown

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+  <Html>
+        <Head>
+          {/* Add your stylesheets here */}
+          <link rel="stylesheet" href="../dist/output.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
   )
 }
