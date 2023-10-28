9ae6430889de635ab2aac621025af2d6a6fd45d7
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-10-28 14:52:27 +0530

    Fixed footer and page numbers

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -3,20 +3,32 @@ import Navbar from "../../shared/AppBar";
 import FooterComp from "../../shared/Footer";
 import ComingSoon from "../comingSoon/ComingSoon";
 import curPageNumber from "../../shared/pageNumber";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
+
+let coursePage = "";
 
 const Layout = ({ children }) => {
   console.log(children?.type?.name, " Layout");
-  
+  const paths = usePathname();
+
+  const pathNames = paths.split("/");
+  if (pathNames[1] !== "404") {
+    coursePage = pathNames[2];
+  }
+
   return (
     <>
       <div className='mt-12 mx-20'>
-        {children?.type?.name === "ComingSoon" ? children : 
-        <>
-        <Navbar />
-        {children}
-        <FooterComp />
-        </>
-      }
+        {children?.type?.name === "ComingSoon" ? (
+          children
+        ) : (
+          <>
+            <Navbar />
+            {children}
+            {!coursePage?.includes("Course") && <FooterComp />}
+          </>
+        )}
       </div>
     </>
   );
