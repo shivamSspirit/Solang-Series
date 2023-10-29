76a842072353713ad0c71daa91ab81bd32b4781b
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-10-29 16:45:02 +0530

    Footer made responsive

--- shared/Footer.js
+++ shared/Footer.js
@@ -13,7 +13,7 @@ const FooterComp = () => {
       container
       className='bg-[#0000005C] border border-[rgba(255, 255, 255, 0.36)] mb-5 rounded-2xl text-white'
     >
-      <div className='w-1/3'>
+      <div className='md:w-1/3'>
         <div className='mb-4 text-2xl'>Solidity for Solana</div>
         <div className='mb-10 text-gray-400'>
           Dive into the world of blockchain development with our Solidity for
@@ -21,8 +21,8 @@ const FooterComp = () => {
         </div>
         <div className='text-gray-400'>solidityforsolana@email.com</div>
       </div>
-      <div className='w-1/3 flex flex-col items-end text-gray-400'>
-        <div className='flex mb-10'>
+      <div className='md:w-1/3 flex flex-col items-start text-gray-400'>
+        <div className='flex mb-10 mt-10 md:mt-0'>
           <Discord className='mr-3' />
           <Github className='mr-3' />
           <TwitterX className='mr-3' />
