d440c53dfc559026ee53f28d8f33f8f2e0787199
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-10-31 23:10:53 +0530

    Search page done

--- pages/Search.js
+++ pages/Search.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Search = () => {
+  
+   function handleFocus() {
+     searchInput.current.blur(); // removing focus
+   }
+
   return (
-    <div className="my-20">
-      <div className="text-3xl font-semibold">Search from the material</div>
+    <div className=''>
+      <div className='text-3xl my-20 font-semibold'>
+        Search from the material
+      </div>
+      <div>
+        <input
+          className='text-white focus:ring-0 focus:border-white bg-transparent w-full border-0 border-b-2 border-b-white'
+          type='text'
+          placeholder='Enter query word'
+          name=''
+          id=''
+        />
+      </div>
     </div>
   );
 };
