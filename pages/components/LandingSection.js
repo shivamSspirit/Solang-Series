2f79cd59635111a11cb6d6e65c5b7d416b4f40de
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-10-31 15:56:38 +0530

    Button hover fixed, card done

--- pages/components/LandingSection.js
+++ pages/components/LandingSection.js
@@ -5,7 +5,6 @@ import InActiveLeftArrow from "../../assets/inactiveLeftIcon.svg";
 import illustration from "../../assets/illustration.png";
 import landingPg1 from "../../assets/landingPg1.png";
 import landingPg2 from "../../assets/landingPg2.png";
-import landingPg3 from "../../assets/landingPg1.png";
 import Ufo from "../../assets/Ufo.svg";
 import Image from "next/image";
 
@@ -16,17 +15,16 @@ import FooterComp from "../../shared/Footer";
 // import Markdown from "react-markdown";
 // import CourseMap from "./CourseMap";
 
-
 const LandingSection = () => {
-  const [curBtn, setCurBtn] = useState(1);
+  // const [curBtn, setCurBtn] = useState(0);
+  const [svgHoverColor, setSvgHoverColor] = useState('white')
 
-  const startBtnOnClick = () => {
-    setCurBtn(1);
-  };
-  const resumeBtnOnClick = () => {
-    setCurBtn(2);
-  };
-
+  // const startBtnOnClick = () => {
+  //   setCurBtn(1);
+  // };
+  // const resumeBtnOnClick = () => {
+  //   setCurBtn(2);
+  // };
 
   const announcementData = [
     {
@@ -40,14 +38,14 @@ const LandingSection = () => {
       desc: "In this part, we will familiarize ourselves with the practicalities of taking the course.",
     },
     {
-      img: landingPg3,
+      img: landingPg1,
       head: "Module Name",
       desc: "In this part, we will familiarize ourselves with the practicalities of taking the course.",
     },
   ];
 
   const module = (
-    <Box sx={{ flexGrow: 1, color: "white"}}>
+    <Box sx={{ flexGrow: 1, color: "white" }}>
       <Grid
         container
         className='flex justify-evenly items-center'
@@ -55,7 +53,7 @@ const LandingSection = () => {
       >
         {announcementData.map((announcement, index) => (
           <Grid
-            className='bg-[rgba(0, 0, 0, 0.16)] my-2 border border-[rgba(255, 255, 255, 0.36)] rounded-2xl'
+            className='bg-[rgba(0, 0, 0, 0.16)] my-2 rounded-2xl'
             item
             xs={3}
             sm={4}
@@ -68,20 +66,6 @@ const LandingSection = () => {
               <div className='px-3 rounded-b-2xl'>
                 <div className='text-2xl pt-6 pb-1'>{announcement.head}</div>
                 <div className='text-[#CECECE] pb-6'>{announcement.desc}</div>
-              </div>
-              <div className='mb-2'>
-                <Button className='w-full bg-white'>
-                  <div
-                    // onClick={() => {
-                    //   router.push({
-                    //     pathname: `/CourseContent/Course${index + 1}`,
-                    //   });
-                    // }}
-                    className='text-center font-bold text-black text-xl'
-                  >
-                    Start Module
-                  </div>
-                </Button>
               </div>
             </div>
           </Grid>
@@ -119,37 +103,43 @@ const LandingSection = () => {
   const timelineData = [
     {
       heading: "2017",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
     {
       heading: "2016",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
     {
       heading: "2015",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
     {
       heading: "2011",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
     {
       heading: "2012",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
     {
       heading: "2007",
-      content: "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam."
+      content:
+        "Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.",
     },
-  ]
+  ];
 
   const timeLine = (
-    <div className="timeline">
+    <div className='timeline'>
       {timelineData.map((data, key) => (
-        <div key={key} className="wrap left">
-          <div className="content bg-[#201F1F] text-white">
+        <div key={key} className='wrap left'>
+          <div className='content bg-[#201F1F] text-white'>
             <h2>{data.heading}</h2>
-            <p className="text-[#CECECE]">{data.content}</p>
+            <p className='text-[#CECECE]'>{data.content}</p>
           </div>
         </div>
       ))}
@@ -198,16 +188,28 @@ const LandingSection = () => {
                 <div className='pr-6'>
                   <button
                     color='transparent'
-                    onClick={startBtnOnClick}
-                    className={`border border-white flex justify-center items-center py-2 px-2 rounded-md ${
-                      curBtn === 1 && "bg-white text-black"
-                    }`}
+                    onMouseEnter={() => setSvgHoverColor("black")}
+                    onMouseLeave={() => setSvgHoverColor("white")}
+                    // onClick={startBtnOnClick}
+                    className={`border border-white flex justify-center items-center py-2 px-2 rounded-md hover:bg-white hover:text-black`}
                   >
-                    {curBtn == 1 ? (
-                      <ActiveLeftArrow className='mr-3' />
-                    ) : (
-                      <InActiveLeftArrow className='mr-3' />
-                    )}
+                    <svg
+                      width='16'
+                      height='17'
+                      viewBox='0 0 16 17'
+                      className='mr-3'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <g id='.left-icon'>
+                        <path
+                          id='Vector'
+                          d='M5.06187 12.9259L0 8.5L5.06187 4.07406L5.92594 5.06187L2 8.5L5.92594 11.9381L5.06187 12.9259ZM10.9381 12.9259L10.0741 11.9381L14 8.5L10.0741 5.06187L10.9381 4.07406L16 8.5L10.9381 12.9259ZM6.94219 14.3125L5.6875 13.94L9.05781 2.6875L10.3125 3.06L6.94219 14.3125Z'
+                          fill={svgHoverColor}
+                        />
+                      </g>
+                    </svg>
+
                     <span className='text-xs md:text-base'>Start Course</span>
                   </button>
                 </div>
