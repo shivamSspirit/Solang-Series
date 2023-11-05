48ef3fcc4304dee24622b4608032790e94829fde
shivam <shivam.spirite3@outlook.com>
2023-11-05 14:06:39 +0530

    generated new

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-5UP7RFEL.mjs
@@ -0,0 +1,101 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import GithubSlugger from "github-slugger";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+  },
+  headings: {
+    type: "json",
+    resolve: async (doc) => {
+      const regXHeader = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+      const slugger = new GithubSlugger();
+      const headings = Array.from(doc.body.raw.matchAll(regXHeader)).map(
+        ({ groups }) => {
+          const flag = groups?.flag;
+          const content = groups?.content;
+          return {
+            level: flag.length,
+            text: content,
+            slug: content ? slugger.slug(content) : void 0
+          };
+        }
+      );
+      return headings;
+    }
+  }
+};
+var Doc = defineDocumentType(() => ({
+  name: "Doc",
+  filePathPattern: `**/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    author: {
+      type: "string"
+    },
+    moduleNumber: {
+      type: "string"
+    },
+    modulePart: {
+      type: "string"
+    },
+    thisFileName: {
+      type: "string"
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "modules",
+  documentTypes: [Doc],
+  md: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section"
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Doc,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-5UP7RFEL.mjs.map
